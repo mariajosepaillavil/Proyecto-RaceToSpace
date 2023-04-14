b9f842002d9eed01a62e9f67ac1359cf4a469773
scastil95 <125077906+scastil95@users.noreply.github.com>
2023-04-14 18:01:00 -0400

    consumo estudiantes API

--- FrontEnd-React/src/components/TablaAlumno.js
+++ FrontEnd-React/src/components/TablaAlumno.js
@@ -1,17 +1,18 @@
 import React from "react";
 const TablaAlumno = ({ Estudiante }) => {
     return (
-        <div className='col-md-6 p-3 col-sm-12 rounded-3 shadow'>
-            <div className='d-flex justify-content-center m-2'>
-                <h3>Lista</h3>
+        <div className='col-md-6 p-5 col-sm-12 rounded-3 shadow' style={{}}>
+            <div className='d-flex justify-content-center m-2' >
+                <h3>Libro de clases</h3>
             </div>
             <div>
                 <table class="table">
                     <thead>
                         <tr className='bg-light text-dark'>
-                            <th scope="col">Nombre Completo</th>
-                            <th scope="col">Curso</th>
-                            <th scope="col">Nombre Módulo</th>
+                            <th scope="col" style={{color:"black"}}>Nombre Completo</th>
+                            <th scope="col" style={{color:"black"}}>Email</th>
+                            <th scope="col" style={{color:"black"}}>Curso</th>
+                       
                         </tr>
                     </thead>
                     <tbody>
@@ -20,9 +21,7 @@ const TablaAlumno = ({ Estudiante }) => {
                                 <th scope="row">{e.estudiantes_nombre}</th>
                                 <td>{e.estudiantes_email}</td>
                                 <td>{e.estudiantes_curso}</td>
-                                <td>{e.modulos_id}</td>
                                          
-                              
                             </tr>
                         ))}
                     </tbody>
