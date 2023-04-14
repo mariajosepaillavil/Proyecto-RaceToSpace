e90bc096514b4cc0ff261f9e3e3f910153c40723
Mariajosepaillavil <maria.paillavilt@utem.cl>
2023-04-13 22:06:08 -0400

    FIX REPOSITORY ESTUDIANTE

--- controladoresRaceToSpace/springbootracetospace/src/main/java/com/racetospace/springbootracetospace/repository/EstudianteRepository.java
+++ controladoresRaceToSpace/springbootracetospace/src/main/java/com/racetospace/springbootracetospace/repository/EstudianteRepository.java
@@ -1,18 +1,49 @@
 package com.racetospace.springbootracetospace.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.racetospace.springbootracetospace.model.Estudiante;
 
-public interface EstudianteRepository extends JpaRepository<Estudiante, Integer>{
+import java.util.List;
+
+
+ 
+//Vas a crear un archivo repositorio en donde cuando te conectes a la base
+//vas a trabajar con objetos de tipo Equipo, con ID tipo Integer
+
+//cambiar class, por interface, es decir, una conjunción de instrucciones
+public interface EstudianteRepository extends JpaRepository <Estudiante,Integer> {
+
+    //Esta es una Query Nativa, sin parámetros que nos permite obtener
+    //Los estudiantes que contengan una LL en su nombre
+    //Es una consulta personalizada
+
+    @Query(value="SELECT * FROM estudiantes WHERE estudiantes_nombre LIKE ('%LL%')",nativeQuery=true)
+
+    List <Estudiante> findEstudiantesWithLL ();
+
+    //Es otra Query Nativa, con parámetros que nos permite obtener
+    //Los estudiantes pertenecientes a un equipo
+    //El cual yo voy a específicar cuando llame a la consulta
+    //También es una consulta personalizada
+
 
     @Query(value="SELECT * FROM estudiantes WHERE modulos_id=?1",nativeQuery=true)
 
+    
+
+    // Ejemplo 2: @Query(value="SELECT * FROM estudiantes WHERE equipo_id=?1 AND nombre=?2",nativeQuery=true)
+   
     List <Estudiante> findEstudiantesByModulo (Integer modulo);
+    //Ejemplo 2: List <Estudiantes> finEstudiantesByEquipo (Integer equipo, String nombre);
 
-   @Query(value="SELECT * FROM estudiantes WHERE estudiantes_nombre LIKE ('%LL%')",nativeQuery=true)
 
-    List <Estudiante> findEstudiantesWithLL ();
+//***** COMO HACER UNA FUNCIÓN DE MODIFICACIÓN CON QUERYS NATIVAS --> EJEMPLO DE OTRO PROYECTO *******
+        //@Modifying
+//@Query(value = "UPDATE PRODUCT p SET p.STOCK = p.stock+10 WHERE p.id = ?1", nativeQuery = true)
+        //void sumarStock(Long recetaID); */
+    
+
 }
+
